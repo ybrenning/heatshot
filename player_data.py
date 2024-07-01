5e0d86a20d1a3b5d30aaf17963eee8dbd58252ea
ybrenning <yannickbrenning2@gmail.com>
2024-07-01 21:04:04 +0200

    Reformat

--- player_data.py
+++ player_data.py
@@ -1,84 +1,84 @@
 player_data = {
     "curryst01": {
-            "Position": "Point Guard",
-            "Shoots": "Right",
-            "Height": "188cm",
-            "Weight": "83kg",
-    }
+        "Position": "Point Guard",
+        "Shoots": "Right",
+        "Height": "188cm",
+        "Weight": "83kg",
+    },
     "antetgi01": {
-            "Position": "Power Forward",
-            "Shoots": "Right",
-            "Height": "211cm",
-            "Weight": "109kg",
-    }
+        "Position": "Power Forward",
+        "Shoots": "Right",
+        "Height": "211cm",
+        "Weight": "109kg",
+    },
     "jamesle01": {
-            "Position": "Small Forward",
-            "Shoots": "Right",
-            "Height": "206cm",
-            "Weight": "113kg"
+        "Position": "Small Forward",
+        "Shoots": "Right",
+        "Height": "206cm",
+        "Weight": "113kg"
     },
     "doncilu01": {
-            "Position": "Point Guard, Shooting Guard",
-            "Shoots": "Right",
-            "Height": "201cm",
-            "Weight": "104kg"
+        "Position": "Point Guard, Shooting Guard",
+        "Shoots": "Right",
+        "Height": "201cm",
+        "Weight": "104kg"
     },
     "jokicni01": {
-            "Position": "Center",
-            "Shoots": "Right",
-            "Height": "211cm",
-            "Weight": "128kg"
+        "Position": "Center",
+        "Shoots": "Right",
+        "Height": "211cm",
+        "Weight": "128kg"
     },
     "gilgesh01": {
-            "Position": "Shooting Guard",
-            "Shoots": "Right",
-            "Height": "198cm",
-            "Weight": "81kg"
+        "Position": "Shooting Guard",
+        "Shoots": "Right",
+        "Height": "198cm",
+        "Weight": "81kg"
     },
-        "embiijo01": {
-            "Position": "Center",
-            "Shoots": "Right",
-            "Height": "213cm",
-            "Weight": "127kg"
-        },
-        "duranke01": {
-            "Position": "Power Forward, Small Forward",
-            "Shoots": "Right",
-            "Height": "211cm",
-            "Weight": "108kg"
-        },
+    "embiijo01": {
+        "Position": "Center",
+        "Shoots": "Right",
+        "Height": "213cm",
+        "Weight": "127kg"
+    },
+    "duranke01": {
+        "Position": "Power Forward, Small Forward",
+        "Shoots": "Right",
+        "Height": "211cm",
+        "Weight": "108kg"
+    },
     "irvinky01": {
-            "Position": "Point Guard, Shooting Guard",
-            "Shoots": "Right",
-            "Height": "188cm",
-            "Weight": "88kg"
-        },
+        "Position": "Point Guard, Shooting Guard",
+        "Shoots": "Right",
+        "Height": "188cm",
+        "Weight": "88kg"
+    },
     "edwaran01": {
-            "Position": "Shooting Guard",
-            "Shoots": "Right",
-            "Height": "193cm",
-            "Weight": "102kg"
-        },
-        "georgpa01": {
-            "Position": "Small Forward, Shooting Guard",
-            "Shoots": "Right",
-            "Height": "203cm",
-            "Weight": "99kg"
-        },
-        "bookede01": {
-            "Position": "Shooting Guard",
-            "Shoots":" Right",
-            "Height": "198cm",
-            "Weight":" 93kg"
-        },
-        "willizi01": {
-            "Position": "Power Forward",
-            "Shoots": "Left",
-            "Height": "198cm",
-            "Weight": "128kg"
-        },
-        "tatumja01": {
-            "Position": "Small Forward, Power Forward",
+        "Position": "Shooting Guard",
+        "Shoots": "Right",
+        "Height": "193cm",
+        "Weight": "102kg"
+    },
+    "georgpa01": {
+        "Position": "Small Forward, Shooting Guard",
+        "Shoots": "Right",
+        "Height": "203cm",
+        "Weight": "99kg"
+    },
+    "bookede01": {
+        "Position": "Shooting Guard",
+        "Shoots": "Right",
+        "Height": "198cm",
+        "Weight": "93kg"
+    },
+    "willizi01": {
+        "Position": "Power Forward",
+        "Shoots": "Left",
+        "Height": "198cm",
+        "Weight": "128kg"
+    },
+    "tatumja01": {
+        "Position": "Small Forward, Power Forward",
         "Shoots": "Right",
         "Height": "203cm",
         "Weight": "95kg"
@@ -96,31 +96,31 @@ player_data = {
         "Weight": "104kg"
     },
     "goberru01": {
-            "Position": "Center",
+        "Position": "Center",
         "Shoots": "Right",
         "Height": "216cm",
         "Weight": "117kg"
     },
     "wembavi01": {
-            "Position": "Center, Power Forward",
+        "Position": "Center, Power Forward",
         "Shoots": "Right",
         "Height": "224cm",
         "Weight": "94kg"
     },
     "derozde01": {
-            "Position": "Shooting Guard",
+        "Position": "Shooting Guard",
         "Shoots": "Right",
         "Height": "198cm",
         "Weight": "99kg"
     },
     "youngtr01": {
-            "Position": "Point Guard",
+        "Position": "Point Guard",
         "Shoots": "Right",
         "Height": "185cm",
         "Weight": "74kg"
     },
     "hardeja01": {
-            "Position": "Point Guard, Shooting Guard",
+        "Position": "Point Guard, Shooting Guard",
         "Shoots": "Left",
         "Height": "196cm",
         "Weight": "99kg"
